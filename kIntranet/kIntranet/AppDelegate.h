aefe0944869185287af4079a6a6cc5022e04a563
Peter Sbarski <peter.sbarski@gmail.com>
2012-09-11 17:10:11 +1000

    Initial commit of files

---
+++ kIntranet/kIntranet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  kIntranet
+//
+//  Created by Peter Sbarski on 11/09/12.
+//  Copyright (c) 2012 Peter Sbarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ kIntranet/kIntranet/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  kIntranet
+//
+//  Created by Peter Sbarski on 11/09/12.
+//  Copyright (c) 2012 Peter Sbarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
