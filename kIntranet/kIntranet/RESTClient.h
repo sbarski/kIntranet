b2d8dffd939f4da585bfc3baa5e84d528b85c7db
Peter Sbarski <peter.sbarski@gmail.com>
2012-09-19 17:30:46 +1000

    Added rest client 	modified:   kIntranet/kIntranet.xcodeproj/project.pbxproj 	modified:   kIntranet/kIntranet/AppDelegate.h 	modified:   kIntranet/kIntranet/AppDelegate.m 	modified:   kIntranet/kIntranet/RESTClient.h 	modified:   kIntranet/kIntranet/RESTClient.m

---
+++ kIntranet/kIntranet/RESTClient.h
@@ -0,0 +1,13 @@
+//
+//  RESTClient.h
+//  kIntranet
+//
+//  Created by Peter Sbarski on 19/09/12.
+//  Copyright (c) 2012 Peter Sbarski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RESTClient : NSObject
+
+@end
---
+++ kIntranet/kIntranet/RESTClient.m
@@ -0,0 +1,13 @@
+//
+//  RESTClient.m
+//  kIntranet
+//
+//  Created by Peter Sbarski on 19/09/12.
+//  Copyright (c) 2012 Peter Sbarski. All rights reserved.
+//
+
+#import "RESTClient.h"
+
+@implementation RESTClient
+
+@end
